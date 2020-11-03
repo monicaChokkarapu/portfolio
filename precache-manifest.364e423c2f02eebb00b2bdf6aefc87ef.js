0d1d92350ac8cd7ec730e248b3001151eea5230c
SRAVAN KUMAR SOMA <sravanksoma@SRAVANs-MBP.attlocal.net>
2020-11-02 22:56:12 -0800

    Updates

---
+++ precache-manifest.364e423c2f02eebb00b2bdf6aefc87ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b515a62c0fc9e9f299a3d654f72749",
+    "url": "/monicaChokkarapu/portfolio/index.html"
+  },
+  {
+    "revision": "28b809bcbf075744ea06",
+    "url": "/monicaChokkarapu/portfolio/static/css/2.60972750.chunk.css"
+  },
+  {
+    "revision": "d36c982b741f573c0343",
+    "url": "/monicaChokkarapu/portfolio/static/css/main.8cf9f879.chunk.css"
+  },
+  {
+    "revision": "28b809bcbf075744ea06",
+    "url": "/monicaChokkarapu/portfolio/static/js/2.a7dd413b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/monicaChokkarapu/portfolio/static/js/2.a7dd413b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36c982b741f573c0343",
+    "url": "/monicaChokkarapu/portfolio/static/js/main.8fea3169.chunk.js"
+  },
+  {
+    "revision": "0cdaaf80e5d1aa133ff9",
+    "url": "/monicaChokkarapu/portfolio/static/js/runtime-main.2cb614a6.js"
+  },
+  {
+    "revision": "5a147418d33fc2b7c46b04381df53665",
+    "url": "/monicaChokkarapu/portfolio/static/media/coding_laptop.5a147418.jpg"
+  },
+  {
+    "revision": "52d9f87b93f862bc9471a0b096d631ce",
+    "url": "/monicaChokkarapu/portfolio/static/media/dark_keyboard.52d9f87b.jpg"
+  }
+]);
